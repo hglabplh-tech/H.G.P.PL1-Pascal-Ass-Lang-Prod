a91810f52bd1cd847bca0d1e78e58d4add3d7b41
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-02-05 21:21:54 +0100

    first trial --- untested

--- PL_ASS_Runtime/src/main/java/hgp/lang/runtime/calculationandtypes/NumericLogic.java
+++ PL_ASS_Runtime/src/main/java/hgp/lang/runtime/calculationandtypes/NumericLogic.java
@@ -1,8 +1,12 @@
 package hgp.lang.runtime.calculationandtypes;
 
+import clojure.lang.Obj;
 import hgp.lang.runtime.calculationandtypes.StackToken.DataTypeId;
 
-import java.util.List;
+import java.util.*;
+
+import static hgp.lang.runtime.calculationandtypes.NumericExpression.*;
+import static hgp.lang.runtime.calculationandtypes.StackToken.*;
 
 /**
  * This class contains the logic to calculate simple terms as well as complex terms+
@@ -13,68 +17,127 @@ public class NumericLogic {
 
     private final StackMachine machine;
 
+    private final Stack<PreCalc> stack = new Stack<>();
+
+
+    private static Map<CommandCode, Integer> operatorPrecedence = new HashMap<>();
+    static {
+        operatorPrecedence.put(CommandCode.MUL, 10);
+        operatorPrecedence.put(CommandCode.DIVIDE, 9);
+        operatorPrecedence.put(CommandCode.MODULO, 7);
+        operatorPrecedence.put(CommandCode.ADD, 5);
+        operatorPrecedence.put(CommandCode.SUBTRACT, 3);
+        ;
+
+    }
+
     public NumericLogic() {
         machine = new StackMachine();
     }
 
 
     public Result calculateTerm(NumericExpression expression) {
-        NumericExpression.ComplexExpression complex = expression.getExpression();
-        NumericExpression.SimpleExpression simple = expression.getSimpleExpression();
+        stack.clear();
+        ComplexExpression complex = expression.getExpression();
+        List<PreCalc> resultsList = new ArrayList<>();
         Result theResult = null;
         if (complex != null) {
-            theResult = calculateComplexExpression(complex);
-        } else if (simple != null) {
-            theResult = computeSimpleExpression(simple);
-
+            resultsList.addAll(calculateComplexExpression(complex));
         }
         return theResult;
     }
 
-    private Result computeSimpleExpression(NumericExpression.SimpleExpression simple) {
-        Result theResult;
+    private PreCalc computeSimpleExpression(PreCalc                                          simple) {
+
         StackToken result = this.getMachine()
-                .pushToStackAndExecute(simple.getOpCode(), simple.getFirstVal(),
-                        simple.getSecondVal(), simple.getResultType());
-        theResult = new Result(result.getType(), result.getValue());
+                .pushToStackAndExecute(simple.getOpCode(), simple.getFirstValue(),
+                        simple.getSecondValue(), simple.getTypeId());
+        PreCalc theResult = new PreCalc(result.getOperation(), result.getType(),
+                null,result.getValue());
         return theResult;
     }
 
-    private Result calculateComplexExpression(NumericExpression.ComplexExpression complex) {
-        Result result = null;
-        NumericExpression.SimpleExpression simpleExpr = complex.getSimpleExpression();
+    private List<PreCalc> calculateComplexExpression(ComplexExpression complex) {
+        List<PreCalc> resList = new ArrayList<>();
+        SimpleExpression simpleExpr = complex.getSimpleExpression();
         List<Expression> simpleExprList = complex.getSimpleExpressions();
         if  (simpleExprList != null && !simpleExprList.isEmpty()) {
             Result prevResult = new Result(simpleExprList.get(0).getTypeId(), 0); // change this
             for (Expression theExpr : simpleExprList) {
-                if (theExpr instanceof NumericExpression.SimpleExpression) {
-                    NumericExpression.SimpleExpression expression =
-                            (NumericExpression.SimpleExpression)theExpr;
-                    result = this.computeSimpleExpression(expression);
-                    NumericExpression.SimpleExpression connectExpr =
-                            new NumericExpression.SimpleExpression(expression.getTheConnectOperation(),
-                                    expression.getOpCode(), expression.getResultType(),
-                                    prevResult.getTheValue(), result.getTheValue());
-                    result = this.computeSimpleExpression(connectExpr);
-                    prevResult = result;
-                } else if (theExpr instanceof NumericExpression.ComplexExpression) {
-                    NumericExpression.ComplexExpression expression =
-                            (NumericExpression.ComplexExpression)theExpr;
-                        result = this.calculateComplexExpression(expression);
+                if (theExpr instanceof SimpleExpression) {
+                  resList.addAll(calculateByPrecedence(simpleExprList));
+                } else if (theExpr instanceof ComplexExpression) {
+                    ComplexExpression expression =
+                            (ComplexExpression)theExpr;
+                        this.calculateComplexExpression(expression);
+
                     // here also will be more logic
                 }
 
             }
         }
         if (simpleExpr != null) {
-            result =  this.computeSimpleExpression(simpleExpr);
+            //result =  this.computeSimpleExpression(simpleExpr);
         }
-        return result;
+        return resList;
+    }
+
+    public List<PreCalc> calculateByPrecedence(List<Expression> expressionList) {
+        Expression  saveExpression = null;
+        List<PreCalc> resList = new ArrayList<>();
+        for (Expression theExpr : expressionList) {
+            PreCalc preCalc = new PreCalc(theExpr.getTheConnectOperation(),
+                    theExpr.getTypeId(), ((SimpleExpression)theExpr).getFirstVal(),
+                    ((SimpleExpression)theExpr).getSecondVal() );
+            if (saveExpression != null) {
+                if (operatorPrecedence.get(theExpr.getTheConnectOperation()) >
+                        operatorPrecedence.get(saveExpression.getTheConnectOperation())) {
+                    resList.add(this.computeSimpleExpression(preCalc));
+                } else {
+                    this.stack.push(preCalc);
+                }
+            }
+            saveExpression = theExpr;
+        }
+        return resList;
     }
 
 
     public StackMachine getMachine() {
         return machine;
+    }
+
+    public static class PreCalc {
+
+        private final CommandCode opCode;
+        private final DataTypeId typeId;
+        private final Number firstValue;
+
+        private final Number secondValue;
+
+        public PreCalc(CommandCode opCode, DataTypeId typeId,
+                       Number firstValue, Number secondValue) {
+            this.opCode = opCode;
+            this.typeId = typeId;
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+        }
+
+        public CommandCode getOpCode() {
+            return opCode;
+        }
+
+        public DataTypeId getTypeId() {
+            return typeId;
+        }
+
+        public Number getFirstValue() {
+            return firstValue;
+        }
+
+        public Number getSecondValue() {
+            return secondValue;
+        }
     }
 
     public static class Result {
@@ -94,4 +157,6 @@ public class NumericLogic {
             return theValue;
         }
     }
+
+
 }
